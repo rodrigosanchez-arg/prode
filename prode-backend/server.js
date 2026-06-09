59e2a9250df3727aba1d11cac4eda5571426b1c8
rodrigosanchez-arg <rodrigo.sanchez@castellanividrios.com.ar>
2026-06-09 13:13:52 -0300

    Update server.js

--- prode-backend/server.js
+++ prode-backend/server.js
@@ -7,6 +7,6 @@ const PORT = process.env.PORT || 4000;
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('🍃 Conectado exitosamente a MongoDB');
-    app.listen(PORT, () => console.log(`🚀 Servidor corriendo en el puerto ${PORT}`));
+    app.listen(PORT, "0.0.0.0", () => console.log(`🚀 Servidor corriendo en el puerto ${PORT}`));
   })
   .catch(err => console.error('Error de conexión a la base de datos:', err));
